9cf80c3e193b7addfcf51e0235e562ceaa303a5b
tuchiiiy <atuecho@gmail.com>
2025-10-04 10:42:03 +0300

    add everything

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+# Define base directory first
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    """Define app configurations"""
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess'
+
+    # Database configuration
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from app import app, db
+from app.models import User
+
+# This function enables easy access to the database instance (db) and User model in the Flask shell session.
+def make_shell_context():
+    return dict(db=db, User=User)
